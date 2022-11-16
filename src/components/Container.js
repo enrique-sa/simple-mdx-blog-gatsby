a09ab575e282b61ffa5a7296e5661c44b5ccf901
enrique-sa <enriquesantollo24@gmail.com>
2022-11-15 23:10:45 -0800

    Add navbar

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { ContainerWrapper } from "../elements";
+import { Navbar } from "../components"
 
 export const Container = ({children}) => {
   return (
     <ContainerWrapper>
+      <Navbar/>
       {children}
     </ContainerWrapper>
   )
